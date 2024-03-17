4136db74d286ce520286798205f12eb408c6f16e
RazeenRamzy <73276893+RazeenRamzy@users.noreply.github.com>
2024-03-17 13:40:45 +0530

    added java files

---
+++ Pro1.java
@@ -0,0 +1,20 @@
+
+
+//Syntax of creating Java Class = public (key word) + class (key word) + Name of the class (we can give any name ex: Pro1) then { is must }
+
+public class Pro1{
+
+
+    public static void main(String[] args) {
+
+        //syntax of  creating output = System. (key word) + out. (key word) + println (key word)
+        //Short key of creating System.out.println is (sout + Enter)
+
+        System.out.println("Hello World");
+        System.out.println("Hi Batch 87");
+
+    }
+
+
+
+}
